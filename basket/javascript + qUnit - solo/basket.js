e594d3a14be2fd2f28dae1e0c4a715df8f5bf88e
tazos333 <tazos333@gmail.com>
2014-11-23 09:07:58 +0100

    allow to add products and check it's count and price

--- basket/javascript + qUnit - solo/basket.js
+++ basket/javascript + qUnit - solo/basket.js
@@ -1,17 +1,24 @@
 var basket = (function(){
 	return function(){
 		var 
+			items = [],
 			getItems = function(){
-				return [];
+				return items;
 			},
 			getPrice = function(){
-				return 0;
+				var price = 0;
+				for(var i =0, max = items.length; i< max; i ++){
+					price += items[i].price;
+				}
+				return price;
 			},
-
-
+			add = function(item){
+				items.push(item);
+			},
 		 	self = {
 				getItems : getItems,
-				getPrice : getPrice
+				getPrice : getPrice,
+				add: add
 			};
 		return self;
 	};
--- basket/javascript + qUnit - solo/basketTests.js
+++ basket/javascript + qUnit - solo/basketTests.js
@@ -16,3 +16,12 @@ QUnit.test("When I create new basket then it should be notNull, empty and price 
 	var price = newBasket.getPrice();
 	assert.equal(0, price);
 });
+
+QUnit.test("When I add one product to basket then one product should be in basket and price should be correct", function( assert ) {
+	var newBasket = basket();
+	var product = {price : 10};
+	newBasket.add(product);
+
+	assert.equal(1, newBasket.getItems().length);
+	assert.equal(10, newBasket.getPrice());
+});
