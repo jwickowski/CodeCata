431f585ea88b3701b5716198b3d2489cea148520
tazos333 <tazos333@gmail.com>
2014-11-23 08:27:38 +0100

    basket should exist

--- basket/javascript + qUnit - solo/basketTests.js
+++ basket/javascript + qUnit - solo/basketTests.js
@@ -1,16 +1,8 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
-
-QUnit.test( "basket exists", function( assert ) {
+QUnit.test( "basket should exist", function( assert ) {
 if(basket) {
   assert.ok( true, "Passed!" );
   }
   else{
    assert.ok( false, "Fail!" );
   }
-});
-
-QUnit.test( "fail test", function( assert ) {
-   assert.ok( false, "Fail!" );
 });
