c4524bdbe001d69ee5439a98c40f4a09b41e7eae
tazos333 <tazos333@gmail.com>
2014-11-23 08:43:51 +0100

    create test for new clear basket

--- basket/javascript + qUnit - solo/basket.js
+++ basket/javascript + qUnit - solo/basket.js
@@ -1,1 +1,10 @@
-var basket = {}
+var basket = (function(){
+	return function(){
+
+		var self = {
+			getItems : function(){return [];},
+			getPrice : function(){return 0;}
+		};
+		return self;
+	};
+})();
--- basket/javascript + qUnit - solo/basketTests.js
+++ basket/javascript + qUnit - solo/basketTests.js
@@ -1,8 +1,18 @@
-QUnit.test( "basket should exist", function( assert ) {
-if(basket) {
-  assert.ok( true, "Passed!" );
-  }
-  else{
-   assert.ok( false, "Fail!" );
-  }
+QUnit.test("basket should exist", function( assert ) {
+	if(basket) {
+  		assert.ok( true, "Passed!" );
+  	}
+  		else{
+   		assert.ok( false, "Fail!" );
+	}
+});
+
+QUnit.test("When I create new basket then it should be notNull, empty and price should be 0", function( assert ) {
+	var newBasket = basket();
+	assert.ok(newBasket);
+	var items = newBasket.getItems();
+	assert.ok(items);
+	assert.equal(0, items.length);
+	var price = newBasket.getPrice();
+	assert.equal(0, price);
 });
